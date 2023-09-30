8e1d3405499a42e4e500805a1b55836da4d5ee21
Samiya Mohsin <127376011+sa07m@users.noreply.github.com>
2023-09-30 14:44:44 +0530

    add product with router

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express')
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 // in order to use req.body, we need the parsed data
 const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:false}))
 
+app.use('/admin',adminRoutes)
+app.use('/shop',shopRoutes)
+
 // app.use((req, res, next)=>{
 //     console.log('this always runs!')
 //     next();
 // })
 
-app.use('/add-product',(req, res, next)=>{   
-    res.send('<form action = "/product" method = "POST"><input type="text" name="title"><input type="text" name="size" placeholder="size"><button type="submit">Add product</button></form>')
+app.get('/',(req, res, next)=>{
+    res.send('<h1>Hello from Express Home</h1>')
 })
-// now we have to add a MW for /product
-app.use('/product', (req, res, next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/',(req, res, next)=>{
-    res.send('<h1>Hello from Express</h1>')
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(3000)
